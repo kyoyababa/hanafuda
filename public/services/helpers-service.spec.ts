791a8d990c292e1ae7ecd522cc38a378d515c6a9
kyoyababa <amour.le.jour.ideal@gmail.com>
2020-04-05 20:06:04 +0900

    isTanzaku

--- public/services/helpers-service.spec.ts
+++ public/services/helpers-service.spec.ts
@@ -53,3 +53,46 @@ describe('isKasu', () => {
     });
   });
 });
+
+describe('isTanzaku', () => {
+  describe('短冊が5枚ある場合', () => {
+    const cards = [
+      { name: '赤短', flowerType: '松', point: 5 },
+      { name: '赤短', flowerType: '梅', point: 5 },
+      { name: '赤短', flowerType: '桜', point: 5 },
+      { name: '短冊', flowerType: '藤', point: 5 },
+      { name: '短冊', flowerType: '菖蒲', point: 5 },
+    ];
+
+    it('true を返すこと', () => {
+      const actual = Helpers.isTanzaku(<Array<Card>>cards);
+      expect(actual).toBeTruthy();
+    });
+  });
+
+  describe('短冊以外が5枚ある場合', () => {
+    const cards = [
+      { name: '鶴', flowerType: '松', point: 20 },
+      { name: '鴬', flowerType: '梅', point: 10 },
+      { name: '幕', flowerType: '桜', point: 20 },
+      { name: '不如帰', flowerType: '藤', point: 10 },
+      { name: 'カス.1', flowerType: '藤', point: 1 },
+    ];
+
+    it('false を返すこと', () => {
+      const actual = Helpers.isKasu(<Array<Card>>cards);
+      expect(actual).toBeFalsy();
+    });
+  });
+
+  describe('札が5枚未満の場合', () => {
+    const cards = [
+      { name: 'カス.1', flowerType: '松', point: 1 },
+    ];
+
+    it('false を返すこと', () => {
+      const actual = Helpers.isKasu(<Array<Card>>cards);
+      expect(actual).toBeFalsy();
+    });
+  });
+});
--- public/services/helpers-service.ts
+++ public/services/helpers-service.ts
@@ -5,3 +5,9 @@ export function isKasu(cards: Array<Card>): boolean {
   const kasuCards = cards.filter(c => c.point === 1);
   return kasuCards.length >= 10;
 }
+
+export function isTanzaku(cards: Array<Card>): boolean {
+  if (cards.length < 5) return false;
+  const tanzakuCards = cards.filter(c => c.point === 5);
+  return tanzakuCards.length >= 5;
+}

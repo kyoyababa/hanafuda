c58013f8bcebc201c38a48dc31f37e9d84992d27
kyoyababa <amour.le.jour.ideal@gmail.com>
2020-04-05 20:18:49 +0900

    リファクタリング：isKasu/ isTanzaku/ isTane

--- public/services/helpers-service.ts
+++ public/services/helpers-service.ts
@@ -1,19 +1,36 @@
 import { Card } from './cards-service';
 
+function checkMatchedBasicYaku(cards: Array<Card>, yaku: 'Kasu' | 'Tanzaku' | 'Tane') {
+  const filterPoint = (): number => {
+    switch(yaku) {
+      case 'Kasu': return 1;
+      case 'Tanzaku': return 5;
+      case 'Tane': return 10;
+    }
+  }
+
+  const filterCardsLength = (): number => {
+    switch(yaku) {
+      case 'Kasu':
+        return 10;
+      case 'Tanzaku':
+      case 'Tane':
+        return 5;
+    }
+  }
+
+  const filteredCards = cards.filter(c => c.point === filterPoint());
+  return filteredCards.length >= filterCardsLength();
+}
+
 export function isKasu(cards: Array<Card>): boolean {
-  if (cards.length < 10) return false;
-  const kasuCards = cards.filter(c => c.point === 1);
-  return kasuCards.length >= 10;
+  return checkMatchedBasicYaku(cards, 'Kasu');
 }
 
 export function isTanzaku(cards: Array<Card>): boolean {
-  if (cards.length < 5) return false;
-  const tanzakuCards = cards.filter(c => c.point === 5);
-  return tanzakuCards.length >= 5;
+  return checkMatchedBasicYaku(cards, 'Tanzaku');
 }
 
 export function isTane(cards: Array<Card>): boolean {
-  if (cards.length < 5) return false;
-  const taneCards = cards.filter(c => c.point === 10);
-  return taneCards.length >= 5;
+  return checkMatchedBasicYaku(cards, 'Tane');
 }

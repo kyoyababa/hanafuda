1902e3c71cc80d874a1abbfcf711b817b862b6d6
kyoyababa <amour.le.jour.ideal@gmail.com>
2020-04-17 19:45:05 +0900

    同じ月の札が明示されるようにした

--- public/index.ts
+++ public/index.ts
@@ -2,6 +2,7 @@ import * as $ from 'jquery';
 
 // import functions via scripts resources
 import * as Enums from "../resources/constants/enums";
+import { FlowerType } from './services/cards-service';
 
 // import components
 import { Header } from './components/header/header';
@@ -41,14 +42,35 @@ export class Index {
 
     shuffledCards.forEach((c: Card, i: number) => {
       if (i < 8) {
+        // 場八
         $bafuda.append(this.generateCardElement(c));
       } else if (i < 8 + 8) {
+        // 手八
         $tefuda.append(this.generateCardElement(c));
       } else if (i < 8 + 8 + 8) {
         // 相手の手札になるので、これらのカードは何もしない
       } else {
         $yamafuda.append(this.generateCardElement(c));
       }
+    });
+
+    this.enableCardSelection();
+  }
+
+  private enableCardSelection(): void {
+    $('#jsi-tefuda > li').hover(function() {
+      const flowerType = $(this).attr('data-flowerType');
+
+      if (!flowerType) return;
+
+      $('#jsi-bafuda > li, #jsi-tefuda > li')
+        .removeClass('is-matched-flower-type');
+      $(`#jsi-bafuda > li[data-flowerType="${flowerType}"], #jsi-tefuda > li[data-flowerType="${flowerType}"]`)
+        .addClass('is-matched-flower-type');
+
+    }, function() {
+      $('#jsi-bafuda > li, #jsi-tefuda > li')
+        .removeClass('is-matched-flower-type');
     });
   }
 }
